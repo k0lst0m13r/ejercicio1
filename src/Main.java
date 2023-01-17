694348abd125d65ddcb88569ed258bf2312cc6b4
Julio Branda <kolstomier@protonmail.com>
2023-01-17 15:01:13 -0300

    Funcion ejercicio 3

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        suma(3, 4, 7);
+
+    }
+
+    public static void suma(int a,  int b, int c){
+        int resultado;
+        resultado = a + b + c;
+
+        System.out.println(resultado);
+    }
+
+}
