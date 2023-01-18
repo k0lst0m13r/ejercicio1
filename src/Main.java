0c16446a7b55589d87cdb15a579484cebbbba4fe
Julio Branda <kolstomier@protonmail.com>
2023-01-18 12:14:20 -0300

    Funcion ejercicio 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+    public void main(String[] args) {
         suma(3, 4, 7);
-
+        Coche miCoche = new Coche();
+        miCoche.agregarPuerta();
+        System.out.println(miCoche.agregarPuerta());
     }
 
     public static void suma(int a,  int b, int c){
@@ -9,6 +11,14 @@ public class Main {
         resultado = a + b + c;
 
         System.out.println(resultado);
+    }
+    class Coche {
+        public int puertas = 3;
+        public int agregarPuerta(){
+
+            return this.puertas ++;
+        }
+
     }
 
 }
