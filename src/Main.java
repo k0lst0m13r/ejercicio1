d518bda20b181558a0a812e2c30c8d5b5dee3b23
Julio Branda <kolstomier@protonmail.com>
2023-01-18 12:54:07 -0300

    ejercicio terminado

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,6 @@ public class Main {
     public static void suma(int a,  int b, int c){
         int resultado;
         resultado = a + b + c;
-
         System.out.println(resultado);
     }
     static class Coche {
